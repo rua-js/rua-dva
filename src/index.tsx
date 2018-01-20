c3f2be78bd48392c991665e06cec14060d780984
copydog <775150@qq.com>
2018-01-21 00:05:37 +0800

    0.4.0-beta.1

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,11 @@
 // Third-party Dependency
 // import invariant from 'invariant'
+import * as React from 'react'
+// @ts-ignore
+import { create } from 'dva-core'
+import { Provider, connect } from 'react-redux'
+// @ts-ignore
+import global from 'global'
 
 // Self Dependency
 import RuaDva from './RuaDva'
@@ -38,8 +44,34 @@ const ruaDva = (dva: any): boolean => {
  */
 const actions = dvaInstance.actions
 
+const dvaLite = (options: any) => {
+  // create dva
+  const app = create(options)
+  // rua dva
+  ruaDva(app)
+  // avoid duplicate registration
+  if (!global.registered) options.models.forEach((model: any) => app.model(model))
+  global.registered = true
+  // old start
+  app.start()
+  // store
+  const store = app._store
+
+  // new start
+  app.start = (container: any) => () => (
+    <Provider store={store}>
+      {container}
+    </Provider>
+  )
+
+  app.getStore = () => store
+
+  return app
+}
+
 
 export {
   ruaDva,
   actions,
+  dvaLite,
 }
