463f4b2b59fd5fd2731c227f1a22bed5b8cc9742
copydog <775150@qq.com>
2018-01-28 14:01:41 +0800

    separating dva-lite

---
+++ src/dva/dvaLite.tsx
@@ -0,0 +1,34 @@
+
+import * as React from 'react'
+// @ts-ignore
+import { create } from 'dva-core'
+import { Provider, connect } from 'react-redux'
+// @ts-ignore
+import * as global from 'global'
+
+const dvaLite = (ruaDva: Function) => (options: any) => {
+  // create dva
+  const app = create(options)
+  // rua dva
+  ruaDva(app)
+  // avoid duplicate registration
+  if (!global.registered) options.models.forEach((model: any) => app.model(model))
+  global.registered = true
+  // old start
+  app.start()
+  // store
+  const store = app._store
+
+  // new start
+  app.start = (container: any) => () => (
+    <Provider store={store}>
+      {container}
+    </Provider>
+  )
+
+  app.getStore = () => store
+
+  return app
+}
+
+export default dvaLite
---
+++ src/dva/index.ts
@@ -0,0 +1,7 @@
+import dvaLite from './dvaLite'
+import dvaReactRouter4 from './dvaReactRouter4'
+
+export {
+  dvaLite,
+  dvaReactRouter4,
+}
