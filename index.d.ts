df85a7787fb71343cbbb3e35602affdaea65d984
copydog <775150@qq.com>
2018-05-13 00:01:24 +0800

    update index.d.ts

--- index.d.ts
+++ index.d.ts
@@ -1,4 +1,5 @@
-import { Actions } from './lib/Types/index';
-export declare const ruaDva: (dva: any) => boolean;
-export declare const actions: Actions;
-export declare const dvaLite: (options: any) => any;
+import { Actions } from './lib/Types';
+declare const actions: Actions;
+declare const dvaLite: (options: any) => any;
+declare const dva: (options: any) => any;
+export { actions, dva, dvaLite };
